8979fc8c30b46f98dcb5861796fe9d7d6d3a688e
Atak Oskonbaev <atakoskonbaev21@gmail.com>
2024-11-29 23:35:04 +0600

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,27 +8,27 @@ public class Main {
     public static void main(String[] args) {
         EmployeeData employeeData = new EmployeeData();
 
-        Employee newEmployee = new Employee("John Snow", "Night's Watch", 0.0, new Date());
+        Employee newEmployee = new Employee("Atak Oskonbaev", "CEO Axolotl", 1000000, new Date());
         employeeData.createEmployee(newEmployee);
         System.out.println("Employee created: " + newEmployee);
 
-        Employee employee = employeeData.getEmployeeById(1);
-        System.out.println("Employee with ID 1: " + employee);
+//        Employee employee = employeeData.getEmployeeById(1);
+//        System.out.println("Employee with ID 1: " + employee);
 
-        List<Employee> employees = employeeData.getAllEmployees();
-        System.out.println("All Employees:");
-        for (Employee emp : employees) {
-            System.out.println(emp);
-        }
+//        List<Employee> employees = employeeData.getAllEmployees();
+//        System.out.println("All Employees:");
+//        for (Employee emp : employees) {
+//            System.out.println(emp);
+//        }
+//
+//        if (employee != null) {
+//            employee.setPosition("Senior Software Engineer");
+//            employee.setSalary(85000.00);
+//            employeeData.updateEmployee(employee);
+//            System.out.println("Updated Employee: " + employee);
+//        }
 
-        if (employee != null) {
-            employee.setPosition("Senior Software Engineer");
-            employee.setSalary(85000.00);
-            employeeData.updateEmployee(employee);
-            System.out.println("Updated Employee: " + employee);
-        }
-
-        employeeData.deleteEmployee(2);
-        System.out.println("Employee with ID 1 deleted.");
+//        employeeData.deleteEmployee(1);
+//        System.out.println("Employee with ID 1 deleted.");
     }
 }
